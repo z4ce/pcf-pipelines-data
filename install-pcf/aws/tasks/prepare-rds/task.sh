d5356194fbc60209601d95250c11e46fd9b9e8bf
Ian Zink <izink@pivotal.io>
2018-02-23 22:03:50 -0600

    Lower case prepare-rds

--- install-pcf/aws/tasks/prepare-rds/task.sh
+++ install-pcf/aws/tasks/prepare-rds/task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 eval "$(goyamlenv config/aws.yml)"
-eval "$(goyamlenv <(echo "$secrets"))"
+eval "$(goyamlenv <(echo "$secrets"))"w
 
 echo "$PEM" > pcf.pem
 chmod 0600 pcf.pem
@@ -16,53 +16,53 @@ cat > databases.sql <<EOF
 CREATE DATABASE IF NOT EXISTS console;
 
 CREATE DATABASE IF NOT EXISTS locket;
-CREATE USER IF NOT EXISTS '$DB_LOCKET_USERNAME' IDENTIFIED BY '$DB_LOCKET_PASSWORD';
-GRANT ALL ON locket.* TO '$DB_LOCKET_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_locket_username' IDENTIFIED BY '$db_locket_password';
+GRANT ALL ON locket.* TO '$db_locket_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS silk;
-CREATE USER IF NOT EXISTS '$DB_SILK_USERNAME' IDENTIFIED BY '$DB_SILK_PASSWORD';
+CREATE USER IF NOT EXISTS '$db_silk_username' IDENTIFIED BY '$db_silk_password';
 GRANT ALL ON silk.* TO '$DB_SILK_USERNAME'@'%';
 
 CREATE DATABASE IF NOT EXISTS uaa;
-CREATE USER IF NOT EXISTS '$DB_UAA_USERNAME' IDENTIFIED BY '$DB_UAA_PASSWORD';
-GRANT ALL ON uaa.* TO '$DB_UAA_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_uaa_username' IDENTIFIED BY '$db_uaa_password';
+GRANT ALL ON uaa.* TO '$db_uaa_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS ccdb;
-CREATE USER IF NOT EXISTS '$DB_CCDB_USERNAME' IDENTIFIED BY '$DB_CCDB_PASSWORD';
-GRANT ALL ON ccdb.* TO '$DB_CCDB_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_ccdb_username' IDENTIFIED BY '$db_ccdb_password';
+GRANT ALL ON ccdb.* TO '$db_ccdb_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS notifications;
-CREATE USER IF NOT EXISTS '$DB_NOTIFICATIONS_USERNAME' IDENTIFIED BY '$DB_NOTIFICATIONS_PASSWORD';
-GRANT ALL ON notifications.* TO '$DB_NOTIFICATIONS_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_notifications_username' IDENTIFIED BY '$db_notifications_password';
+GRANT ALL ON notifications.* TO '$db_notifications_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS autoscale;
-CREATE USER IF NOT EXISTS '$DB_AUTOSCALE_USERNAME' IDENTIFIED BY '$DB_AUTOSCALE_PASSWORD';
-GRANT ALL ON autoscale.* TO '$DB_AUTOSCALE_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_autoscale_username' IDENTIFIED BY '$db_autoscale_password';
+GRANT ALL ON autoscale.* TO '$db_autoscale_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS app_usage_service;
-CREATE USER IF NOT EXISTS '$DB_APP_USAGE_SERVICE_USERNAME' IDENTIFIED BY '$DB_APP_USAGE_SERVICE_PASSWORD';
-GRANT ALL ON app_usage_service.* TO '$DB_APP_USAGE_SERVICE_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_app_usage_service_username' IDENTIFIED BY '$db_app_usage_service_password';
+GRANT ALL ON app_usage_service.* TO '$db_app_usage_service_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS routing;
-CREATE USER IF NOT EXISTS '$DB_ROUTING_USERNAME' IDENTIFIED BY '$DB_ROUTING_PASSWORD';
-GRANT ALL ON routing.* TO '$DB_ROUTING_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_routing_username' IDENTIFIED BY '$db_routing_password';
+GRANT ALL ON routing.* TO '$db_routing_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS diego;
-CREATE USER IF NOT EXISTS '$DB_DIEGO_USERNAME' IDENTIFIED BY '$DB_DIEGO_PASSWORD';
-GRANT ALL ON diego.* TO '$DB_DIEGO_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_diego_username' IDENTIFIED BY '$db_diego_password';
+GRANT ALL ON diego.* TO '$db_diego_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS account;
-CREATE USER IF NOT EXISTS '$DB_ACCOUNTDB_USERNAME' IDENTIFIED BY '$DB_ACCOUNTDB_PASSWORD';
-GRANT ALL ON account.* TO '$DB_ACCOUNTDB_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_accountdb_username' IDENTIFIED BY '$db_accountdb_password';
+GRANT ALL ON account.* TO '$db_accountdb_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS nfsvolume;
-CREATE USER IF NOT EXISTS '$DB_NFSVOLUMEDB_USERNAME' IDENTIFIED BY '$DB_NFSVOLUMEDB_PASSWORD';
-GRANT ALL ON nfsvolume.* TO '$DB_NFSVOLUMEDB_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_nfsvolumedb_username' IDENTIFIED BY '$db_nfsvolumedb_password';
+GRANT ALL ON nfsvolume.* TO '$db_nfsvolumedb_username'@'%';
 
 CREATE DATABASE IF NOT EXISTS networkpolicyserver;
-CREATE USER IF NOT EXISTS '$DB_NETWORKPOLICYSERVERDB_USERNAME' IDENTIFIED BY '$DB_NETWORKPOLICYSERVERDB_PASSWORD';
-GRANT ALL ON networkpolicyserver.* TO '$DB_NETWORKPOLICYSERVERDB_USERNAME'@'%';
+CREATE USER IF NOT EXISTS '$db_networkpolicyserverdb_username' IDENTIFIED BY '$db_networkpolicyserverdb_password';
+GRANT ALL ON networkpolicyserver.* TO '$db_networkpolicyserverdb_username'@'%';
 EOF
 
 scp -i pcf.pem -o StrictHostKeyChecking=no databases.sql "ubuntu@${OPSMAN_DOMAIN_OR_IP_ADDRESS}:/tmp/."
-ssh -i pcf.pem -o StrictHostKeyChecking=no "ubuntu@${OPSMAN_DOMAIN_OR_IP_ADDRESS}" "mysql -h $db_host -u $db_username -p$db_password < /tmp/databases.sql"
+ssh -i pcf.pem -o StrictHostKeyChecking=no "ubuntu@${opsman_domain_or_ip_address}" "mysql -h $db_host -u $db_username -p$db_password < /tmp/databases.sql"
