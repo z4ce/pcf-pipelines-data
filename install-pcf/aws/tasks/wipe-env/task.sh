15431d5eb12ba0cb748366f5e7b9e769fcb253a2
Ian Zink <izink@pivotal.io>
2018-02-24 00:24:03 -0600

    Remove _id from variable x2

--- install-pcf/aws/tasks/wipe-env/task.sh
+++ install-pcf/aws/tasks/wipe-env/task.sh
@@ -44,7 +44,7 @@ terraform init
 
 terraform destroy \
   -force \
-  -var "aws_access_key_id=${aws_access_key_id}" \
+  -var "aws_access_key_id=${aws_access_key}" \
   -var "aws_secret_access_key=${aws_secret_access_key}" \
   -var "aws_region=${aws_region}" \
   -var "opsman_ami=dontcare" \
